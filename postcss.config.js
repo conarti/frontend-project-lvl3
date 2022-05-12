09ca7b8367e48c807e35f7112f1d162ecd5c3134
Belous MacOS <conarti.dsn@gmail.com>
2022-05-12 22:47:45 +0300

    build(postcss): purgecss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+const getPlugins = () => {
+  const plugins = ['postcss-preset-env'];
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push(purgecss({
+      content: ['./**/*.html'],
+    }));
+  }
+  return plugins;
+};
+
 module.exports = {
   // Add you postcss configuration here
   // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
-  plugins: ['postcss-preset-env'],
+  plugins: getPlugins(),
 };
